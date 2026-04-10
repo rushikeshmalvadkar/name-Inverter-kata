a9de07e32a6bb072e96286bae131f02b9b28c61a
rs <sachin@gmail.com>
2026-04-10 14:16:39 +0530

    Initial commit

---
+++ src/main/java/com/rm/nik/NameInverterKataApplication.java
@@ -0,0 +1,13 @@
+package com.rm.nik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameInverterKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NameInverterKataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rm/nik/NameInverterKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rm.nik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameInverterKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
